8d592714e9c51b13bbeaa4401676803cf47dc658
jdidi94 <jdididaoud404@gmail.com>
2022-03-02 12:43:58 +0100

    form control setup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {ReactiveFormsModule} from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './auth/signup/signup.component';
@@ -16,7 +16,8 @@ import { LandingComponent } from './landing/landing.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/auth/signup/signup.component.ts
+++ client/src/app/auth/signup/signup.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import {  FormControl,FormGroup  } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+ 
+  checkoutForm = new FormGroup({
+    name:new FormControl(''), 
+    lastName: new FormControl(''),
+    email:new FormControl(''),
+    gender:new FormControl(''),
+    password: new FormControl(''),
+    repeatPassword:new FormControl(''),
 
-  constructor() { }
+  });
+  // let nextDiv:<string>
+  constructor() { 
+     
+    }
 
   ngOnInit(): void {
+    let ProgressForm=true
+    let nextDiv=[1,2,3,4]
   }
+  changeDiv(){
 
+  }
+ onSubmit(){
+
+}
 }
