f78dbd2671aa757cb460fa7a373303ce5f78a13a
jdidi94 <jdididaoud404@gmail.com>
2022-05-11 14:48:55 +0200

    form validation

--- client/src/app/auth/signup/signup.component.ts
+++ client/src/app/auth/signup/signup.component.ts
@@ -1,33 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import {  FormControl,FormGroup  } from '@angular/forms';
+import {  FormControl,FormGroup ,Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
- 
+  ProgressForm=true
+  nextDivs:number[]=[1,2,3,4]
+  show:number=1
   checkoutForm = new FormGroup({
-    name:new FormControl(''), 
-    lastName: new FormControl(''),
-    email:new FormControl(''),
-    gender:new FormControl(''),
-    password: new FormControl(''),
-    repeatPassword:new FormControl(''),
+    firstName:new FormControl('',Validators.required), 
+    lastName: new FormControl('',Validators.required),
+    email:new FormControl('',[Validators.required,Validators.email]),
+    number:new FormControl('',Validators.required),
+    dateOfBirth:new FormControl('',Validators.required),
+    gender:new FormControl('',Validators.required),
+    password: new FormControl('',[Validators.required]),
+    repeatPassword:new FormControl('',Validators.required),
 
   });
   // let nextDiv:<string>
   constructor() { 
-     
+  
     }
 
   ngOnInit(): void {
-    let ProgressForm=true
-    let nextDiv=[1,2,3,4]
+ console.log(this.checkoutForm.get("firstName"))
   }
-  changeDiv(){
+  changeDivNext(){
+if(this.show <4&& this.ProgressForm ){
+  this.show++
+  
+}else {
+  this.ProgressForm=false
+}
+  }
+  changeDivPrevious(){
+    if(this.show >1){
+      this.show--
+  console.log("showprev",this.show)
 
-  }
+    }else{
+      this.show=1
+    }
+      }
  onSubmit(){
 
 }
